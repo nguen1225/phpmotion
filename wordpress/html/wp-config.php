9614fbf98f651c2041241d7c001f98a1f829161a
Yuki Mizuta <nguen1225@gmail.com>
2020-12-13 15:23:49 +0900

    first commit

---
+++ wordpress/html/wp-config.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://wordpress.org/support/article/editing-wp-config-php/
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'wordpress_db');
+
+/** MySQL database username */
+define( 'DB_USER', 'admin');
+
+/** MySQL database password */
+define( 'DB_PASSWORD', 'adminadmin');
+
+/** MySQL hostname */
+define( 'DB_HOST', 'db:3306');
+
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         '7f28c5c97e89fd9cc7dacc2a60c23ccc4bbc9602');
+define( 'SECURE_AUTH_KEY',  'ded71cfff1455b2e164e8fc34e5c4354cf836429');
+define( 'LOGGED_IN_KEY',    '19bcd86e214fdab355c478c0829296c58ce1081d');
+define( 'NONCE_KEY',        '9ce92f6fcdc14e881373b83a1c34f49e3dc6ade7');
+define( 'AUTH_SALT',        '05f497ea64a4598b8bd6822f4b8fc1a7a8ce0186');
+define( 'SECURE_AUTH_SALT', '502d9378ecab674a3d6ccb153eda78131a42f59c');
+define( 'LOGGED_IN_SALT',   '590106618ef90253525c3e1e7a10626a6f5a97f1');
+define( 'NONCE_SALT',       'ce62d439fbb10d0977a2c86a0aee6426920f113b');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://wordpress.org/support/article/debugging-in-wordpress/
+ */
+define( 'WP_DEBUG', false );
+
+// If we're behind a proxy server and using HTTPS, we need to alert WordPress of that fact
+// see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+	$_SERVER['HTTPS'] = 'on';
+}
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
---
+++ wordpress/html/wp-content/themes/books/index.php
@@ -0,0 +1,105 @@
+<?php
+
+// メッセージを保存するファイルのパス設定
+define( 'FILENAME', './message.txt');
+
+//タイムゾーン設定
+date_default_timezone_set('Asia/tokyo');
+
+//変数の初期化
+$now_date = null;
+$date = null;
+$file_handle = null;
+$split_date = null;
+$message = array();
+$message_array = array();
+
+if( !empty($_POST['btn_submit']) ) {
+  if( $file_handle = fopen( FILENAME, "a")) {
+    //書き込み日時を取得
+    $now_date = date("Y-m-d H:i:s");
+    //書き込むデータを作成
+    $date = "'".$_POST['view_name']."','".$_POST['message']."','".$now_date."'\n";
+    //書き込み
+    fwrite( $file_handle, $date);
+    //ファイルを閉じる。
+    fclose( $file_handle);
+  }
+  // var_dump($_POST);	
+  
+  if( $file_handle = fopen( FILENAME,'r')) {
+    while( $data = fgets($file_handle) ){
+      $split_date = preg_split( '/\'/', $data);
+      $message = array(
+        'view_name' => $split_date[1],
+        'message' => $split_date[3],
+        'post_date' => $split_date[5]
+      );
+      array_unshift( $message_array, $message);
+      // echo $data . "<be>";
+    }
+    //ファイルを閉じる
+    fclose( $file_handle);
+  }
+}
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="utf-8">
+	<title>超劣化版Ｔｗｉｔｔｅｒ</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">    
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="mystyle.css">
+	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+<h1>超劣化版Ｔｗｉｔｔｅｒ</h1>
+<!-- ここにメッセージの入力フォームを設置 -->
+<div class="messageform" style="margin-left: 20px;">
+  <form method="post">
+    <div>
+      <label for="view_name">name</label>
+      <br>
+      <input id="view_name" type="text" name="view_name" value="">
+    </div>
+    <br>
+    <div>
+      <label for="message">text</label>
+      <br>
+      <textarea id="message" name="message"></textarea>
+    </div>
+    <br>
+    <input type="submit" name="btn_submit" value="ｔｗｅｅｔ">
+  </form>
+  <hr>
+  <section>
+    <?php if( !empty($message_array) ): ?>
+    <?php foreach( $message_array as $value ): ?>
+    <article>
+      <div class="info">
+        <h2>
+          <?php echo $value['view_name']; ?>
+        </h2>
+        <time>
+          <?php echo date('y年m月d日 H:i', strtotime($value['post_date'])); ?>
+        </time>
+      </div>
+      <p>
+        <?php echo $value['message']; ?>
+      </p>
+    </article>
+    <?php endforeach; ?>
+    <?php endif; ?>
+  </section>
+</div>
+  <hr>
+
+	<footer>
+		<div class="container">
+			<p class="text-center">Copyright ©  books.</p>
+		</div>
+	</footer>		
+</body>
+</html>
